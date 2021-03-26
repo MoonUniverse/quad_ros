0ced894f389dac808ab302d37f445c4d2b7c2766
yuying.jin <yuying.jin@tmirob.com>
2021-03-26 14:23:55 +0800

    usb camera driver

--- navigation/monocular_pose_estimator/monocular_pose_estimator/src/monocular_pose_estimator.cpp
+++ navigation/monocular_pose_estimator/monocular_pose_estimator/src/monocular_pose_estimator.cpp
@@ -29,29 +29,30 @@
 
 #include "monocular_pose_estimator/monocular_pose_estimator.h"
 
-namespace monocular_pose_estimator
-{
+namespace monocular_pose_estimator {
 
 /**
  * Constructor of the Monocular Pose Estimation Node class
  *
  */
-MPENode::MPENode(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private)
-  : nh_(nh), nh_private_(nh_private), have_camera_info_(false)
-{
+MPENode::MPENode(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private)
+    : nh_(nh), nh_private_(nh_private), have_camera_info_(false) {
   // Set up a dynamic reconfigure server.
   // This should be done before reading parameter server values.
-  dynamic_reconfigure::Server<monocular_pose_estimator::MonocularPoseEstimatorConfig>::CallbackType cb_;
+  dynamic_reconfigure::Server<
+      monocular_pose_estimator::MonocularPoseEstimatorConfig>::CallbackType cb_;
   cb_ = boost::bind(&MPENode::dynamicParametersCallback, this, _1, _2);
   dr_server_.setCallback(cb_);
 
   // Initialize subscribers
-  image_sub_ = nh_.subscribe("/usb_cam/image_raw", 1, &MPENode::imageCallback, this);
-  camera_info_sub_ = nh_.subscribe("/usb_cam/camera_info", 1, &MPENode::cameraInfoCallback, this);
-  ROS_INFO("Helloworld");
+  image_sub_ =
+      nh_.subscribe("/usb_cam/image_raw", 1, &MPENode::imageCallback, this);
+  camera_info_sub_ = nh_.subscribe("/usb_cam/camera_info", 1,
+                                   &MPENode::cameraInfoCallback, this);
 
   // Initialize pose publisher
-  pose_pub_ = nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>("estimated_pose", 1);
+  pose_pub_ = nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>(
+      "estimated_pose", 1);
 
   // Initialize image publisher for visualization
   image_transport::ImageTransport image_transport(nh_);
@@ -62,18 +63,15 @@ MPENode::MPENode(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private)
 
   // Read in the marker positions from the YAML parameter file
   XmlRpc::XmlRpcValue points_list;
-  if (!nh_private_.getParam("marker_positions", points_list))
-  {
-    ROS_ERROR(
-        "%s: No reference file containing the marker positions, or the file is improperly formatted. Use the 'marker_positions_file' parameter in the launch file.",
-        ros::this_node::getName().c_str());
+  if (!nh_private_.getParam("marker_positions", points_list)) {
+    ROS_ERROR("%s: No reference file containing the marker positions, or the "
+              "file is improperly formatted. Use the 'marker_positions_file' "
+              "parameter in the launch file.",
+              ros::this_node::getName().c_str());
     ros::shutdown();
-  }
-  else
-  {
+  } else {
     positions_of_markers_on_object.resize(points_list.size());
-    for (int i = 0; i < points_list.size(); i++)
-    {
+    for (int i = 0; i < points_list.size(); i++) {
       Eigen::Matrix<double, 4, 1> temp_point;
       temp_point(0) = points_list[i]["x"];
       temp_point(1) = points_list[i]["y"];
@@ -83,17 +81,15 @@ MPENode::MPENode(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private)
     }
   }
   trackable_object_.setMarkerPositions(positions_of_markers_on_object);
-  ROS_INFO("The number of markers on the object are: %d", (int )positions_of_markers_on_object.size());
+  ROS_INFO("The number of markers on the object are: %d",
+           (int)positions_of_markers_on_object.size());
 }
 
 /**
  * Destructor of the Monocular Pose Estimation Node class
  *
  */
-MPENode::~MPENode()
-{
-
-}
+MPENode::~MPENode() {}
 
 /**
  * The callback function that retrieves the camera calibration information
@@ -101,10 +97,8 @@ MPENode::~MPENode()
  * \param msg the ROS message containing the camera calibration information
  *
  */
-void MPENode::cameraInfoCallback(const sensor_msgs::CameraInfo::ConstPtr& msg)
-{
-  if (!have_camera_info_)
-  {
+void MPENode::cameraInfoCallback(const sensor_msgs::CameraInfo::ConstPtr &msg) {
+  if (!have_camera_info_) {
     cam_info_ = *msg;
 
     // Calibrated camera
@@ -123,44 +117,44 @@ void MPENode::cameraInfoCallback(const sensor_msgs::CameraInfo::ConstPtr& msg)
     have_camera_info_ = true;
     ROS_INFO("Camera calibration information obtained.");
   }
-
 }
 
 /**
- * The callback function that is executed every time an image is received. It runs the main logic of the program.
+ * The callback function that is executed every time an image is received. It
+ * runs the main logic of the program.
  *
  * \param image_msg the ROS message containing the image to be processed
  */
-void MPENode::imageCallback(const sensor_msgs::Image::ConstPtr& image_msg)
-{
+void MPENode::imageCallback(const sensor_msgs::Image::ConstPtr &image_msg) {
 
   // Check whether already received the camera calibration data
-  if (!have_camera_info_)
-  {
+  if (!have_camera_info_) {
     ROS_WARN("No camera info yet...");
     return;
   }
 
   // Import the image from ROS message to OpenCV mat
   cv_bridge::CvImagePtr cv_ptr;
-  try
-  {
-    cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::MONO8);
-  }
-  catch (cv_bridge::Exception& e)
-  {
+  try {
+    cv_ptr =
+        cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::MONO8);
+  } catch (cv_bridge::Exception &e) {
     ROS_ERROR("cv_bridge exception: %s", e.what());
     return;
   }
   cv::Mat image = cv_ptr->image;
 
-  // Get time at which the image was taken. This time is used to stamp the estimated pose and also calculate the position of where to search for the makers in the image
+  // Get time at which the image was taken. This time is used to stamp the
+  // estimated pose and also calculate the position of where to search for the
+  // makers in the image
   double time_to_predict = image_msg->header.stamp.toSec();
 
-  const bool found_body_pose = trackable_object_.estimateBodyPose(image, time_to_predict);
-  if (found_body_pose) // Only output the pose, if the pose was updated (i.e. a valid pose was found).
+  const bool found_body_pose =
+      trackable_object_.estimateBodyPose(image, time_to_predict);
+  if (found_body_pose) // Only output the pose, if the pose was updated (i.e. a
+                       // valid pose was found).
   {
-    //Eigen::Matrix4d transform = trackable_object.getPredictedPose();
+    // Eigen::Matrix4d transform = trackable_object.getPredictedPose();
     Matrix6d cov = trackable_object_.getPoseCovariance();
     Eigen::Matrix4d transform = trackable_object_.getPredictedPose();
 
@@ -172,36 +166,31 @@ void MPENode::imageCallback(const sensor_msgs::Image::ConstPtr& image_msg)
     predicted_pose_.pose.pose.position.x = transform(0, 3);
     predicted_pose_.pose.pose.position.y = transform(1, 3);
     predicted_pose_.pose.pose.position.z = transform(2, 3);
-    Eigen::Quaterniond orientation = Eigen::Quaterniond(transform.block<3, 3>(0, 0));
+    Eigen::Quaterniond orientation =
+        Eigen::Quaterniond(transform.block<3, 3>(0, 0));
     predicted_pose_.pose.pose.orientation.x = orientation.x();
     predicted_pose_.pose.pose.orientation.y = orientation.y();
     predicted_pose_.pose.pose.orientation.z = orientation.z();
     predicted_pose_.pose.pose.orientation.w = orientation.w();
 
     // Add covariance to PoseWithCovarianceStamped message
-    for (unsigned i = 0; i < 6; ++i)
-    {
-      for (unsigned j = 0; j < 6; ++j)
-      {
+    for (unsigned i = 0; i < 6; ++i) {
+      for (unsigned j = 0; j < 6; ++j) {
         predicted_pose_.pose.covariance.elems[j + 6 * i] = cov(i, j);
       }
     }
 
     // Publish the pose
     pose_pub_.publish(predicted_pose_);
-  }
-  else
-  { // If pose was not updated
+  } else { // If pose was not updated
     ROS_WARN("Unable to resolve a pose.");
   }
 
   // publish visualization image
-  if (image_pub_.getNumSubscribers() > 0)
-  {
+  if (image_pub_.getNumSubscribers() > 0) {
     cv::Mat visualized_image = image.clone();
     cv::cvtColor(visualized_image, visualized_image, CV_GRAY2RGB);
-    if (found_body_pose)
-    {
+    if (found_body_pose) {
       trackable_object_.augmentImage(visualized_image);
     }
 
@@ -216,22 +205,28 @@ void MPENode::imageCallback(const sensor_msgs::Image::ConstPtr& image_msg)
 }
 
 /**
- * The dynamic reconfigure callback function. This function updates the variable within the program whenever they are changed using dynamic reconfigure.
+ * The dynamic reconfigure callback function. This function updates the variable
+ * within the program whenever they are changed using dynamic reconfigure.
  */
-void MPENode::dynamicParametersCallback(monocular_pose_estimator::MonocularPoseEstimatorConfig &config, uint32_t level)
-{
+void MPENode::dynamicParametersCallback(
+    monocular_pose_estimator::MonocularPoseEstimatorConfig &config,
+    uint32_t level) {
   trackable_object_.detection_threshold_value_ = config.threshold_value;
   trackable_object_.gaussian_sigma_ = config.gaussian_sigma;
   trackable_object_.min_blob_area_ = config.min_blob_area;
   trackable_object_.max_blob_area_ = config.max_blob_area;
-  trackable_object_.max_width_height_distortion_ = config.max_width_height_distortion;
+  trackable_object_.max_width_height_distortion_ =
+      config.max_width_height_distortion;
   trackable_object_.max_circular_distortion_ = config.max_circular_distortion;
   trackable_object_.roi_border_thickness_ = config.roi_border_thickness;
 
-  trackable_object_.setBackProjectionPixelTolerance(config.back_projection_pixel_tolerance);
-  trackable_object_.setNearestNeighbourPixelTolerance(config.nearest_neighbour_pixel_tolerance);
+  trackable_object_.setBackProjectionPixelTolerance(
+      config.back_projection_pixel_tolerance);
+  trackable_object_.setNearestNeighbourPixelTolerance(
+      config.nearest_neighbour_pixel_tolerance);
   trackable_object_.setCertaintyThreshold(config.certainty_threshold);
-  trackable_object_.setValidCorrespondenceThreshold(config.valid_correspondence_threshold);
+  trackable_object_.setValidCorrespondenceThreshold(
+      config.valid_correspondence_threshold);
 
   ROS_INFO("Parameters changed");
 }
