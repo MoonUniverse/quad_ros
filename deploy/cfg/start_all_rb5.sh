6e876c2679009b7dd8da10446588a0d2b9aba678
Yuyingjin <jinyuyingwork@gmail.com>
2021-03-29 21:21:53 +0800

    fix sh bug

--- deploy/cfg/start_all_rb5.sh
+++ deploy/cfg/start_all_rb5.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 roslaunch realsense2_camera realsense_camera.launch &
 
+sleep 25
+
+echo "realsense_camera starting success!"
+
 roslaunch djiros djiros.launch &
+
+sleep 10
+
+echo "djiros starting success!"
 
