4193387c7d6c5b6a0a99d34949fcca7eb75415d6
yuying.jin <jinyuyingsspu@gmail.com>
2023-04-11 17:54:49 +0800

    init

--- deploy/cfg/start_all.sh
+++ deploy/cfg/start_all.sh
@@ -2,7 +2,7 @@
 
 xfce4-terminal  -T master -e "bash -iC 'echo start!; $SHELL'" \
         --tab   -T ros_core -e "bash -ic 'roscore; $SHELL'" \
-        --tab   -T realsense_camera -e "bash -ic 'sleep 3; roslaunch realsense2_camera realsense_camera.launch; $SHELL'" \
-        --tab   -T djiros -e "bash -ic 'sleep 25; roslaunch djiros djiros.launch; $SHELL'" \
-        --tab   -T nlink_parser -e "bash -ic 'sleep 10; roslaunch nlink_parser tofsense.launch; $SHELL'" \
-        --tab   -T happymoon_quad_control -e "bash -ic 'sleep 10; roslaunch happymoon_quad_control happymoon_control.launch; $SHELL'" \
+        --tab   -T realsense_camera -e "bash -ic 'sleep 5; roslaunch realsense2_camera realsense_camera.launch; $SHELL'" \
+        --tab   -T vins_fusion -e "bash -ic 'sleep 35; roslaunch vins vins_fusion.launch; $SHELL'" \
+        --tab   -T vins_fusion -e "bash -ic 'sleep 15; roslaunch vins vins_rviz.launch; $SHELL'" \
+        #--tab   -T happymoon_quad_control -e "bash -ic 'sleep 10; roslaunch happymoon_quad_control happymoon_control.launch; $SHELL'" \
