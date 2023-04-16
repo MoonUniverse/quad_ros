8d237d977e43d67e13aabd1c2dd88b8b35725514
yuying.jin <jinyuyingsspu@gmail.com>
2023-04-16 18:50:58 +0800

    add 200hz imu

--- deploy/cfg/start_all.sh
+++ deploy/cfg/start_all.sh
@@ -4,6 +4,7 @@ xfce4-terminal  -T master -e "bash -iC 'echo start!; $SHELL'" \
         --tab   -T ros_core -e "bash -ic 'roscore; $SHELL'" \
         --tab   -T realsense_camera -e "bash -ic 'sleep 5; roslaunch realsense2_camera rs_camera.launch; $SHELL'" \
         --tab   -T mavros -e "bash -ic 'sleep 5; roslaunch mavros px4.launch; $SHELL'" \
-        --tab   -T vins_fusion -e "bash -ic 'sleep 25; roslaunch vins vins_fusion.launch; $SHELL'" \
+        --tab   -T mavros_200hz -e "bash -ic 'sleep 15; rosrun mavros mavcmd long 511 105 5000 0 0 0 0 0; $SHELL'" \
+        --tab   -T vins_fusion -e "bash -ic 'sleep 35; roslaunch vins vins_fusion.launch; $SHELL'" \
         --tab   -T vins_rviz -e "bash -ic 'sleep 15; roslaunch vins vins_rviz.launch; $SHELL'" \
         --tab   -T happymoon_quad_control -e "bash -ic 'sleep 45; roslaunch happymoon_quad_control happymoon_control.launch; $SHELL'" \
